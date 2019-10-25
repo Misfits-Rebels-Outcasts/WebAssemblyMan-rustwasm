ebd72c41406a0978e4dbd6e46f9c420e5760fac8
WebAssemblyMan <jupiter@DESKTOP-CGQ0ASH.localdomain>
2019-10-25 11:53:26 +0800

    web_sys_console bare minimum

--- rustwasm/web_sys_console/src/lib.rs
+++ rustwasm/web_sys_console/src/lib.rs
@@ -20,10 +20,30 @@ pub fn console_assert_with_condition_and_data() {
     web_sys::console::assert_with_condition_and_data(z<y,&array); 
 }
 
+//Log an Array to the console.
+pub fn console_log() {
+    let array = js_sys::Array::new();
+    array.push(&"Hello Console Log".into());
+    web_sys::console::log(&array);
+}
+
+//Log "Hello World" to the console.
+pub fn console_log1() {
+     web_sys::console::log_1(&"Hello World".into());
+}
+
+//Log "Hello World" with 1 string subsitution to the console.
+pub fn console_log2() {
+    web_sys::console::log_2(&"%s : Hello World".into(),&"John".into());
+}
+
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
     //console_assert();
-    console_assert_with_condition_and_data();
+    //console_assert_with_condition_and_data();
+    console_log();
+    console_log1();
+    console_log2();
     Ok(())
 
 }
