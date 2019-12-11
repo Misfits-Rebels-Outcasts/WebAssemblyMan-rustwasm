0bd87fe854c39014a97437bacecad718ae36aa80
WebAssemblyMan <jupiter@DESKTOP-PM4DFQ0.localdomain>
2019-12-11 19:05:54 +0800

    web_sys_document_create_element

--- rustwasm/web_sys_document/src/lib.rs
+++ rustwasm/web_sys_document/src/lib.rs
@@ -1,14 +1,25 @@
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen(start)]
-pub fn run() -> Result<(), JsValue> {
-
+//Create a paragraph element and append it to the HTML body.
+pub fn document_create_element()
+{
     let window = web_sys::window().expect("global window does not exists");    
     let document = window.document().expect("expecting a document on window");
     let body = document.body().expect("document expect to have have a body");
-    let val = document.create_element("p")?;
+    let val = document.create_element("p").unwrap();
     val.set_inner_html("Hello World from WebAssemblyMan!");
-    body.append_child(&val)?;
+    body.append_child(&val).unwrap();
+}
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    document_create_element();
+}
+/*
+#[wasm_bindgen(start)]
+pub fn run() -> Result<(), JsValue> {
+
     Ok(())
     
 }
+*/
